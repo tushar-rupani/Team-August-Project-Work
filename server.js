2f877e7e84d8d66d77a61e87cf990c03ea4a8fcc
chirag-talpada <chiragvaghela492@gmail.com>
2023-03-17 19:18:09 +0530

    23DEV052: enhanced dashboard

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql2/promise")
-const authRoutes = require("./routes/registerRoutes")
+const mysql = require("mysql2/promise");
+
+//require routes
+const authRoutes = require("./routes/registerRoutes");
+const dashboardRoutes = require("./routes/homeRoutes");
+
+//set template engine
 app.set("view engine", "ejs");
+
+//middleware set up
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
+//define routes
 app.use("/", authRoutes)
+app.use("/dashboard", dashboardRoutes)
 
 app.listen(3000, () => {
     console.log("App is runnig");
