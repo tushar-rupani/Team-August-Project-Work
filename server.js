ea22c9fcb567ce17c83b012d9fe11a13c116fd70
pooja11096 <poojamakwana6356@gmail.com>
2023-03-17 18:31:36 +0530

    23Dev024:backend in tabbed activities

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql2/promise")
 const authRoutes = require("./routes/registerRoutes")
+const employeeDataRoutes = require("./routes/employeeDataHandling");
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
-app.use("/", authRoutes)
+app.use("/", authRoutes);
+
+app.use("/employee-data", employeeDataRoutes);
 
 app.listen(3000, () => {
     console.log("App is runnig");
