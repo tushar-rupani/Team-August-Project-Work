f4ca8c7e7e91cb2d9e9f4261d7a529788612de39
Khushi3081 <rachchh.khushi30@gmail.com>
2023-03-20 15:05:20 +0530

    Commit before merge

--- server.js
+++ server.js
@@ -6,8 +6,17 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
+
+
 app.use("/", authRoutes)
 
+app.get("/attendence",function(req,res) {
+    res.render('attendence.ejs')
+})
+
+app.get("/leaves",function(req,res) {
+    res.render('leaves.ejs');
+})
 app.listen(3001, () => {
     console.log("App is runnig");
 })
