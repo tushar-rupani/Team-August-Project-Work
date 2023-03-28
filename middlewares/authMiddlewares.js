29e79b350e34f82ea5110aef222d7eb04aca6198
chirag talpada <99867142+chirag-talpada@users.noreply.github.com>
2023-03-28 12:28:00 +0530

    Merging Validation Part. (#8)
    * 23DEV052, 23DEV050: Integrated Validation on Employee From, Bug Fix

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -1,41 +1,47 @@
 const jwt = require("jsonwebtoken");
 var connection = require("../connection/connection");
 
-function checkIfTokenExists(req, res, next){
+function checkIfTokenExists(req, res, next) {
     let token = req.cookies.token;
-    if(!token){
+    if (!token) {
         next();
-    }else{
+    } else {
         return res.redirect("/activate");
     }
 }
 
-function handleLogin(req, res, next){
-    if(!req.session.user){
+async function handleLogin(req, res, next) {
+    if (!req.session.user) {
         return res.redirect("/")
     }
     next();
+    
 }
 
-async function checkIfLoggedIn(req, res, next){
-    
-    if(req.session.user){
-        let emp_id=req.session.id;
-        let check_emp_details = `select * from basic_information where employee_id = '${emp_id}'`;
-        try {
-            let [hasData] = await connection.execute(check_emp_details);
 
-            if (hasData.length === 0) {
-                return res.redirect("/employee-form");
-            }
-
-
-        } catch (e) {
-            console.log(e);
-        }
+function checkIfLoggedIn(req, res, next) {
+    if (req.session.user) {
         return res.redirect("/self/home")
     }
     next();
 }
 
-module.exports = {checkIfTokenExists, handleLogin, checkIfLoggedIn};
+async function hasEmployeeData(req, res, next) {
+    let emp_id = req.session.user;
+
+    let check_emp_details = `select * from basic_information where employee_id = '${emp_id}'`;
+    try {
+        let [hasData] = await connection.execute(check_emp_details);
+
+        if (hasData.length === 0) {
+            return res.redirect("/employee-form");
+        }else{
+            return res.redirect("/self/home");
+        }
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+module.exports = { checkIfTokenExists, handleLogin, checkIfLoggedIn };
