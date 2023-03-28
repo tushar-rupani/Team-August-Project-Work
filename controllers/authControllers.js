996f3eb37c30c7405f39c423112a687d1031c13d
chirag talpada <99867142+chirag-talpada@users.noreply.github.com>
2023-03-28 15:43:11 +0530

    Update authControllers.js

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -165,7 +165,7 @@ const registerController = async (req, res) => {
         from: "tushar24081@gmail.com",
         to: `${email}`,
         subject: "Password Activation Link",
-        html: `<h1>Welcome aboard!</h1><p>Click the below activation link to get started.</p><a href="http://localhost:3000/activate-account/${token}">${token}</a>`,
+        html: `<h1>Welcome aboard!</h1><p>Click the below activation link to get started.</p><a href="https://august.appdemoserver.com/${token}">${token}</a>`,
       };
       transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
