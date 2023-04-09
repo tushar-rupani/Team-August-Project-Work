de953da99cae12a5b65d2f118b8074223cb341d4
Tushar Rupani <tushar24081@gmail.com>
2023-04-09 14:44:08 +0530

    23DEV050: Making a notification sound when message comes. (#28)
    * Saving before merge
    
    * 23DEV050: Leave and Attendance Page
    
    * 23DEV050: 24 Hours activity
    
    * Changing Code again
    
    * 23DEV050: Edit From Functionality added
    
    * Commit before pulling march 30
    
    * 23DEV050, 23DEV052: resolved CSS and fixed some issues
    
    * Fixing Link issue
    
    * Fixing Filter issue in leave section
    
    * Changing TimeZone and Mobile Design issue
    
    * 23DEV052: TimeZone changes added
    
    * 23DEV050: resolving authentication issues
    
    * 23DEV050: added sensation using SocketIO
    
    * 23DEV052: Adding Forgot Password Functionality
    
    * Removed Logs
    
    * 23DEV050: Advanced Sensation Module
    
    * 23DEV050: Shows notification when user is away, and detailed leave details
    
    * 23DEV050: allow user to see detailed leave page
    
    * Adding sound when message comes

--- public/js/socket-script.js
+++ public/js/socket-script.js
@@ -13,14 +13,15 @@ const Toast2 = Swal.mixin({
 
 const socket = io();
 socket.on("chat", (data) => {
-    // Trigger a pop-up notification when a new message is received
     const {message, userName} = data;
-    // window.alert("New message: " + message + userName );
-  
+    
+    let notificationSoundUrl = `../assets/notification-sound.mp3`;
     Toast2.fire({
-      icon: 'info', // Set the icon to null
-      title: `${userName}`, // Set the title of the toast notification
-      text: `${message.substr(0, 20)}...` // Set the text of the toast notification
+      icon: 'info',
+      title: `${userName}`, 
+      html: `<audio src="${notificationSoundUrl}" autoplay></audio>`,
+      text: `${message.substr(0, 20)}...`
+
     });
   
     Notification.requestPermission().then((permission) => {
