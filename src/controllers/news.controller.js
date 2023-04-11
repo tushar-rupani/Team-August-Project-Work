10222b018d1834ec99ce10e84f35ba3fc7cccc91
tushar24081 <tushar24081@gmail.com>
2023-04-11 15:18:04 +0530

    23DEV050: Developed a functionality where if user forgets to checkout we ask them a form

---
+++ src/controllers/news.controller.js
@@ -0,0 +1,13 @@
+var connection = require("../connection/connection");
+
+const news = async (req, res) => {
+
+    let query = `select image,title,description from news`;
+    let result = await connection.execute(query);
+    res.render('news', {
+        activatePage: "news",
+        data: result[0]
+    });
+
+}
+module.exports = {news};
--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -4,9 +4,9 @@ const moment = require("moment");
 const jwt = require("jsonwebtoken");
 
 const { handleLogin } = require("../middlewares/authMiddlewares");
-const {renderHome, renderLogs, editForm} = require("../controllers/home.controller")
+const {renderHome, renderLogs, editForm, forgotCheckout} = require("../controllers/home.controller")
 const {checkIndividualEmp} = require("../controllers/employee-data.controller");
-
+const {news} = require("../controllers/news.controller")
 const {
   attendanceGenerate,
   returnSearchData,
@@ -49,6 +49,8 @@ router.get("/profile", handleLogin, profileController);
 
 router.get("/emp-data/:id", handleLogin, checkIndividualEmp);
 
+router.get("/news", handleLogin, news);
 
+router.post("/add-forgot", handleLogin, forgotCheckout)
 
 module.exports = router;
