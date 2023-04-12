bdbb1bc327aab16f9f766e0f202e86b2805bfb21
tushar24081 <tushar24081@gmail.com>
2023-04-12 13:53:54 +0530

    Sending images in socket

--- src/controllers/sensation.controller.js
+++ src/controllers/sensation.controller.js
@@ -2,7 +2,7 @@ var connection = require("../connection/connection");
 const moment = require("moment");
 
 const renderController = async(req, res) => {
-    let getAllMessages = "SELECT s.message, b.full_name, s.time, b.profile_pic FROM sensation s INNER JOIN basic_information b ON b.employee_id = s.emp_id order by s.time";
+    let getAllMessages = "SELECT s.message, b.full_name, s.time, b.profile_pic, s.image FROM sensation s INNER JOIN basic_information b ON b.employee_id = s.emp_id order by s.time";
     try{
         var [executeAllMessage] = await connection.execute(getAllMessages);
     }catch(e){
@@ -12,12 +12,25 @@ const renderController = async(req, res) => {
 }
 
 const postMessage = async(req, res) => {
+    let filename;
+    if(req.files.length){
+        filename = req.files[0].filename;
+    }
     let message = req.body.message;
     let time = moment().format('YYYY-MM-DD HH:mm:ss');
     let currentUser = req.session.user;
-    let sqlQuery = `INSERT INTO sensation (emp_id, message, time) VALUES (${currentUser}, '${message}', '${time}')`;
+    let sqlQuery;
+    if(filename){
+        sqlQuery = `INSERT INTO sensation (emp_id, message, time, image) VALUES (${currentUser}, '${message}', '${time}', '${filename}')`;
+    }else{
+        sqlQuery = `INSERT INTO sensation (emp_id, message, time) VALUES (${currentUser}, '${message}', '${time}')`;
+    }
     try{
         let [fireQuery] = await connection.execute(sqlQuery);
+        console.log(fireQuery);
+        let getSelectQuery = `SELECT * from sensation where id = ${fireQuery.insertId}`;
+        let [executeSelect] = await connection.execute(getSelectQuery);
+        return res.json(executeSelect);
     }catch(e){
         console.log(e);
     }
